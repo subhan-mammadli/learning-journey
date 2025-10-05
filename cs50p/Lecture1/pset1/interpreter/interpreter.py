15f3e10c69381153c91cc4cd69d108295095133e
SubhanMamedoff <subhanmamedoff010@gmail.com>
2025-10-05 14:36:13 +0400

    Add Problem Solution - Math Interpreter

---
+++ cs50p/Lecture1/pset1/interpreter/interpreter.py
@@ -0,0 +1,16 @@
+expression = input("Expression: ")
+
+x , y, z = expression.split(' ')
+
+x = float(x)
+z = float(z)
+
+match y:
+    case "+":
+        print(x + z)
+    case "-":
+        print(x - z)
+    case "*":
+        print(x * z)
+    case "/":
+        print(x / z)
