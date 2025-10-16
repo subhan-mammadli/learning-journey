83ea88c1a1047df212d22842ba8f9bcbe5a9527c
SubhanMamedoff <subhanmamedoff010@gmail.com>
2025-10-16 12:06:56 +0000

    Add Problem Solution - Felipe's Taqueria

---
+++ cs50p/Lecture3/pset3/taqueria/taqueria.py
@@ -0,0 +1,28 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+def main():
+    total = 0
+    while True:
+        try:
+            item_input = input("Item: ").lower()
+            for item in menu:
+                if item_input == item.lower():
+                    total = total + menu[item]
+                    print(f"${total:.2f}")
+        except EOFError:
+            print()
+            break
+
+
+main()
