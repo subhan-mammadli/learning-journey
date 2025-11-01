d6b46ead686eee31ec712b9681e76d5f7dd9660e
Subhan Mamedoff <subhanmamedoff010@gmail.com>
2025-11-01 10:52:17 +0400

    Add Problem Solution and Tests - Back to the Bank

---
+++ cs50p/Lecture5/pset5/test_bank/test_bank.py
@@ -0,0 +1,14 @@
+from bank import value
+
+
+def test_hello():
+    assert value("Hello") == 0
+    assert value("Hello, Newman") == 0
+
+def test_h_but_not_hello():
+    assert value("How you doing?") == 20
+
+
+def test_other():
+
+    assert value("What's happening?") == 100
