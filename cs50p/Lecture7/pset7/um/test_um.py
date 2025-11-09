bc67e9dcbcaecc99579f04189171b0c08c97ace5
Subhan Mamedoff <subhanmamedoff010@gmail.com>
2025-11-09 18:55:59 +0400

    Add Problem Solution and Tests - Regular, um, Expressions

---
+++ cs50p/Lecture7/pset7/um/test_um.py
@@ -0,0 +1,10 @@
+from um import count
+
+
+def test_valid():
+    assert count("um") == 1
+    assert count("um?") == 1
+    assert count("um?") == 1
+    assert count("Um, thanks for the album.") == 1
+    assert count("Um, thanks, um...") == 2
+    assert count("yummy") == 0
